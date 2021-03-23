694c7b40a44b9db6831e94683555cd7ed5cc6dff
KOSON <18818209416@139.com>
2021-03-23 15:04:27 +0800

    [defect NO.]直接加入SDK全部文件-目前编译失败 [modify comment] [bin source]0

---
+++ MDK-ARM/STM32C8T6/mcu_api.d
@@ -0,0 +1,7 @@
+stm32c8t6\mcu_api.o: ..\mcu_sdk\mcu_api.c
+stm32c8t6\mcu_api.o: ..\mcu_sdk\wifi.h
+stm32c8t6\mcu_api.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+stm32c8t6\mcu_api.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+stm32c8t6\mcu_api.o: ..\mcu_sdk\protocol.h
+stm32c8t6\mcu_api.o: ..\mcu_sdk\system.h
+stm32c8t6\mcu_api.o: ..\mcu_sdk\mcu_api.h
---
+++ MDK-ARM/STM32C8T6/system.d
@@ -0,0 +1,7 @@
+stm32c8t6\system.o: ..\mcu_sdk\system.c
+stm32c8t6\system.o: ..\mcu_sdk\wifi.h
+stm32c8t6\system.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+stm32c8t6\system.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+stm32c8t6\system.o: ..\mcu_sdk\protocol.h
+stm32c8t6\system.o: ..\mcu_sdk\system.h
+stm32c8t6\system.o: ..\mcu_sdk\mcu_api.h
